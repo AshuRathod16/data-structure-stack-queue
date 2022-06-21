b5f2b4ade2c30e14467f538d5debabc8548ecbab
Ashwini Rathod <rashwini510@gmail.com>
2022-06-21 18:24:41 +0530

    Added: Insert the element in queue and printed them

--- src/com/bridgelabz/Function.java
+++ src/com/bridgelabz/Function.java
@@ -3,28 +3,17 @@ package com.bridgelabz;
 import java.util.LinkedList;
 
 public class Function {
-    LinkedList<Integer> stack = new LinkedList<Integer>();
+    LinkedList<Integer> queue = new LinkedList<Integer>();
 
-    public void addElement(int data) {
-        stack.addFirst(data);
+    // Method to insert the element in queue
+    public void addQueue(int data) {
+        queue.addFirst(data);
     }
 
-    public void peek() {
-        System.out.println("Element at peek in stack is: " + stack.peek());
-    }
-
-    public void pop() {
-        if (stack.size() == 0) {
-            System.out.println("Stack is empty");
-        } else {
-            System.out.println("Elements removed from stack is: " + stack.pop());
-        }
-
-    }
-
+    // Method to print the element in queue
     public void print() {
         System.out.println("Element in the stack are:");
-        for (Integer i : stack) {
+        for (Integer i : queue) {
             System.out.println(" -> " + i);
         }
     }
--- src/com/bridgelabz/StackAndQueue.java
+++ src/com/bridgelabz/StackAndQueue.java
@@ -5,24 +5,11 @@ public class StackAndQueue {
 
         Function function = new Function();
 
-        // Insert element in stack
-        function.addElement(70);
-        function.addElement(30);
-        function.addElement(56);
+        // Inserting element in queue
+        function.addQueue(70);
+        function.addQueue(30);
+        function.addQueue(56);
         function.print();
-
-        // Removed the element from stack until stack is empty
-        function.peek();
-        function.pop();
-        function.print();
-        function.peek();
-        function.pop();
-        function.print();
-        function.peek();
-        function.pop();
-        function.print();
-        function.peek();
-        function.pop();
 
     }
 }
