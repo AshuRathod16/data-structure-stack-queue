af1d986ebe3340db24d4690f1740bf6d5f316f55
Ashwini Rathod <rashwini510@gmail.com>
2022-06-21 18:10:35 +0530

    Added: Remove the element from stack till it is empty and print it

--- src/com/bridgelabz/Function.java
+++ src/com/bridgelabz/Function.java
@@ -9,6 +9,19 @@ public class Function {
         stack.addFirst(data);
     }
 
+    public void peek() {
+        System.out.println("Element at peek in stack is: " + stack.peek());
+    }
+
+    public void pop() {
+        if (stack.size() == 0) {
+            System.out.println("Stack is empty");
+        } else {
+            System.out.println("Elements removed from stack is: " + stack.pop());
+        }
+
+    }
+
     public void print() {
         System.out.println("Element in the stack are:");
         for (Integer i : stack) {
--- src/com/bridgelabz/StackAndQueue.java
+++ src/com/bridgelabz/StackAndQueue.java
@@ -5,10 +5,24 @@ public class StackAndQueue {
 
         Function function = new Function();
 
+        // Insert element in stack
         function.addElement(70);
         function.addElement(30);
         function.addElement(56);
         function.print();
+
+        // Removed the element from stack until stack is empty
+        function.peek();
+        function.pop();
+        function.print();
+        function.peek();
+        function.pop();
+        function.print();
+        function.peek();
+        function.pop();
+        function.print();
+        function.peek();
+        function.pop();
 
     }
 }
